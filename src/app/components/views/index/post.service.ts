df0a5fb46e9508852980bc94ebd69573158c509c
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-02-08 08:48:30 -0300

    Update upload image

--- src/app/components/views/index/post.service.ts
+++ src/app/components/views/index/post.service.ts
@@ -34,7 +34,7 @@ export class PostService {
 
         const url = `${this.baseUrlPost}/imagens/imagem/add/${idpost}`
         return this.http.post(url,  formDate, {
-        headers: {'Content-type': 'undefined', 'Expect': '100-continue', 'Content-Length': '1234567890987'}});
+        headers: {'Content-type': 'multipart/form-data', 'Expect': '100-continue', 'Content-Length': '1234567890987'}});
     }
   
   mensagem(str: String): void {
